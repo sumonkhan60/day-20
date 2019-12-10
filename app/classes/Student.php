2776861d33e16e6f3b9060151668d1f4c1525cb5
sumonkhan60 <sumonkhan60@yahoo.com>
2019-12-10 18:47:29 +0600

    new file created

---
+++ app/classes/Student.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\classes;
+class Student
+{
+    public function saveStudentInfo($data) {
+        $link = mysqli_connect('localhost', 'root', '', 'bitm');
+
+        $sql = "INSERT INTO students(name, email, mobile) VALUES ('$data[name]', '$data[email]', '$data[mobile]')";
+
+        if (mysqli_query($link, $sql) ) {
+            $message =  "Student info save successfully";
+            return $message;
+        }else{
+            die('Query problem'.mysqli_error($link));
+        }
+    }
+    public function getAllStudentInfo(){
+        $link = mysqli_connect('localhost', 'root', '', 'bitm');
+        $sql = "SELECT * FROM students";
+
+        if(mysqli_query($link, $sql)){
+            $queryResult = mysqli_query($link, $sql);
+
+            return $queryResult;
+        }else {
+            die('Query problem'.mysqli_error($link));
+        }
+    }
+}
